102ebf8d80c8a024cd1220847e542538fd0bac6e
xuhuigithub <1498472791@qq.com>
2017-10-09 23:42:38 +0800

    增加了porject_name模板

---
+++ project_name/project_name.py
@@ -0,0 +1,40 @@
+#!/bin/env python
+#coding:utf-8
+import os
+import sys
+
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG','on') == 'on'
+SCRET_KEY = os.environ.get('SECRET_KEY','{{ secret_key }}')
+#SCRET_KEY = os.environ.get('SECRET_KEY',os.urandom(32))
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost').split(',')
+
+settings.configure(
+  DEBUG=DEBUG,
+  SCRET_KEY = SCRET_KEY,
+  ALLOWED_HOSTS = ALLOWED_HOSTS,
+  ROOT_URLCONF = __name__,
+  MIDDLEWARE_CLASSES=(
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  ),
+)
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+
+def index(request):
+  return HttpResponse('Hello World')
+
+urlpatterns = (
+  url(r'^$',index)
+)
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+  from django.core.management import execute_from_command_line
+  execute_from_command_line(sys.argv)
